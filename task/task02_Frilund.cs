5efde3ab6b499a2e61bcdf3d25d75f52232b820d
AlekseiFrilund <frilundag@gmail.com>
2022-08-07 12:44:44 +0300

    решение зачач

---
+++ task/task02_Frilund.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Фрилунд А.
+
+using static System.Console; 
+Clear(); 
+
+WriteLine("Введите число 1: "); 
+int number1 = Convert.ToInt32 (ReadLine());  
+WriteLine("Введите число 2: "); 
+int number2 = Convert.ToInt32 (ReadLine()); 
+
+if (number1 <= number2)  
+{
+    WriteLine("");
+    WriteLine("Результаты:");
+    WriteLine($"Наибольшее из двух чисел: {number2}"); 
+    WriteLine($"Наименьшее из двух чисел: {number1}"); 
+} 
+        
---
+++ task/task04_Frilund.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Фрилунд А.
+
+using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Clear();
+
+        WriteLine("Введите число 1: ");
+        int number1 = Convert.ToInt32(ReadLine());
+        WriteLine("Введите число 2: ");
+        int number2 = Convert.ToInt32(ReadLine());
+        WriteLine("Введите число 3: ");
+        int number3 = Convert.ToInt32(ReadLine());
+
+        int max = number1;
+
+        if (max < number2) max = number2;
+        if (max < number3) max = number3;
+
+        WriteLine("");
+        WriteLine("Результаты:");
+        WriteLine($"Наибольшем из трех чисел является: {max}");
+    }
+}
